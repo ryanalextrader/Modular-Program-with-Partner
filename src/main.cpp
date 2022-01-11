ecb5a05a5948cfba1fa0f0e184d6a9199e692908
ryanalextrader <ryanalextrader@gmail.com>
2022-01-11 11:37:01 -0800

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <mbed.h>
+#include "HostIO.h"
+#include "SegDisplay.h"
 
 int main() {
 
   // put your setup code here, to run once:
-
+  char data1, data2;
+  SegInit();
+  HostInit();
   while(1) {
     // put your main code here, to run repeatedly:
+    data2 = GetKeyInput();
+    Seg2 = SegConvert(data2);
+    data1 = GetKeyInput();
+    Seg1 = SegConvert(data1);
+    printf(" ");
   }
 }
